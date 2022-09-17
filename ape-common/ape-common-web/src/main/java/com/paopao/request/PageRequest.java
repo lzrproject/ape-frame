1ea38ed91e77dcbbeeb1f64051731d3c16c2c49b
11 <11>
2022-09-17 18:05:17 +0800

    分页工具

---
+++ ape-common/ape-common-web/src/main/java/com/paopao/request/PageRequest.java
@@ -0,0 +1,6 @@
+package com.paopao.request;/**
+ * @Author  111
+ * @Date  2022/9/17 11:11
+ * @Description
+*/public class PageRequest {
+}
---
+++ ape-common/ape-common-web/src/main/java/com/paopao/request/PageResponse.java
@@ -0,0 +1,6 @@
+package com.paopao.request;/**
+ * @Author  111
+ * @Date  2022/9/17 15:05
+ * @Description
+*/public class PageResponse {
+}
