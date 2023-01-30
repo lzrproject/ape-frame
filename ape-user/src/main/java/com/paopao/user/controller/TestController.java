c113e0c22e15673e40934c2ddc061d7aec023657
paopao <894073772@qq.com>
2023-01-30 11:37:32 +0800

    guava测试

--- ape-user/src/main/java/com/paopao/user/controller/TestController.java
+++ ape-user/src/main/java/com/paopao/user/controller/TestController.java
@@ -3,6 +3,7 @@ package com.paopao.user.controller;
 import com.paopao.tool.freemarker.ExportWordUtil;
 import com.paopao.user.entity.User;
 import com.paopao.user.utils.ThreadLocalUtil;
+import com.paopao.util.CacheUtil;
 import com.paopao.util.RedisShareLockUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +12,12 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @Author 111
  * @Date 2022/8/10 16:40
- * @Description
+ * @Description 测试类
  */
 
 @RestController
@@ -33,21 +33,24 @@ public class TestController {
     @Autowired
     private RedisShareLockUtil redisShareLockUtil;
 
+    @Autowired
+    private CacheUtil cacheUtil;
+
     @GetMapping("get")
-    public String test(@RequestParam String value){
+    public String test(@RequestParam String value) {
         System.out.println(ThreadLocalUtil.getCurrentId());
 
         return "Hello World!";
     }
 
     @PostMapping("entity")
-    public void stringTest(@RequestBody User user){
+    public void stringTest(@RequestBody User user) {
         System.out.println(user.getPassword());
     }
 
     @GetMapping("redisTest")
-    public void redisTest(){
-        redisTemplate.opsForValue().set("name","paopao");
+    public void redisTest() {
+        redisTemplate.opsForValue().set("name", "paopao");
     }
 
     @GetMapping("redisLock")
@@ -61,20 +64,41 @@ public class TestController {
     public void testLog() {
         long start = System.currentTimeMillis();
         for (int i = 0; i < 100000; i++) {
-            log.info("这是第{}条日志",i);
+            log.info("这是第{}条日志", i);
         }
         long end = System.currentTimeMillis();
-        log.info("耗时{}",end-start);
+        log.info("耗时{}", end - start);
     }
 
     @GetMapping("testExport")
     public void testExport() {
         Map<String, Object> map = new HashMap<>();
-        map.put("name","张三");
-        map.put("age",21);
-        map.put("body","打篮球");
-        map.put("signName","王五");
-        ExportWordUtil.exportWord(map,"个人资料","aaa.ftl");
+        map.put("name", "张三");
+        map.put("age", 21);
+        map.put("body", "打篮球");
+        map.put("signName", "王五");
+        ExportWordUtil.exportWord(map, "个人资料", "aaa.ftl");
     }
 
+    /**
+     * guava本地缓存
+     */
+    @GetMapping("/localCache")
+    public void localCacheTest() {
+        List<Long> skuIdList = new ArrayList<>();
+        String cachePrefix = "skuInfo.skuName";
+        cacheUtil.getResult(skuIdList, cachePrefix, SkuInfo.class, (list) -> {
+            return getSkuInfo();
+        });
+    }
+
+    public Map<Long,SkuInfo> getSkuInfo() {
+        return Collections.emptyMap();
+    }
+
+    static class SkuInfo {
+        private Long id;
+        private String name;
+        private Long price;
+    }
 }
