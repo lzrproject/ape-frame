06be31f2ba627dc42335e5d6cd9d8228bd31083e
paopao <111@qq.com>
2024-02-02 17:49:06 +0800

    1

--- ape-demo/src/test/java/com/paopao/demo/redisTest/BitTest.java
+++ ape-demo/src/test/java/com/paopao/demo/redisTest/BitTest.java
@@ -1,7 +1,11 @@
 package com.paopao.demo.redisTest;
 
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
 import org.junit.jupiter.api.Test;
 import redis.clients.jedis.Jedis;
+
+import java.nio.charset.StandardCharsets;
 
 /**
  * 模块描述
@@ -11,13 +15,43 @@ import redis.clients.jedis.Jedis;
  */
 public class BitTest {
 
+    /**
+     * Bit
+     */
     @Test
     public void test1() {
         Jedis jedis = new Jedis();
         String key = "bit_test";
         String value = "Hello world";
-        for (char c : value.toCharArray()) {
-            jedis.setbit(key, 0, c)
+        jedis.setbit(key.getBytes(StandardCharsets.UTF_8), 0, value.getBytes());
+        jedis.close();
+    }
+
+    /**
+     * HyperLogLog
+     */
+    @Test
+    public void test2() {
+        Jedis jedis = new Jedis();
+        for (int i = 0; i < 100000; i++) {
+            jedis.pfadd("codehole", "user" + i);
         }
+        long codehole = jedis.pfcount("codehole");
+        System.out.printf("%d %d\n", 100000, codehole);
+        jedis.close();
+    }
+
+    /**
+     * 布隆过滤器
+     */
+    @Test
+    public void test3() {
+        BloomFilter<String> filter = BloomFilter.create(Funnels.stringFunnel(StandardCharsets.UTF_8)
+                , 100, 0.01);
+        filter.put("hello1");
+        filter.put("hello2");
+        filter.put("hello3");
+        System.out.println(filter.mightContain("hello1"));
+        System.out.println(filter.mightContain("hello4"));
     }
 }
